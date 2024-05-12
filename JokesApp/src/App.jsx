1bdda546ea84f20f8150d1d9958a6f179af49ae0
aabhushanCD <dammardhakal426@gmail.com>
2024-05-12 14:53:42 +0545

    all file recovered

---
+++ JokesApp/src/App.jsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import Box from "./components/box";
+import "@fontsource/island-moments";
+function App() {
+  return (
+    <>
+      <div className="jokelogo">
+        <span className="logo">
+          <a href="#" style={{ textDecoration: "none" }}>
+            <span> JokesCenter</span>
+          </a>
+        </span>
+      </div>
+
+      <container>
+        <div className="child1"></div>
+        <div className="child2">
+          <Box></Box>
+          <Box></Box>
+          <Box></Box>
+        </div>
+        <div className="child3"></div>
+      </container>
+      <footer>
+        <p>copyright &copy 2024</p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
